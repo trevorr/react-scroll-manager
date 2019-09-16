de313da724639b7c879308fdc472376f741fecfa
Trevor Robinson <trevor@scurrilous.com>
2019-09-16 08:20:41 -0500

    Add TypeScript definitions
    Thanks to soerenbf for initial version

---
+++ src/index.d.ts
@@ -0,0 +1,18 @@
+import React from 'react';
+import { History } from 'history';
+
+export interface ScrollManagerProps {
+  history: History;
+  sessionKey?: string;
+  timeout?: number;
+}
+
+export class ScrollManager extends React.Component<ScrollManagerProps> { }
+
+export class WindowScroller extends React.Component { }
+
+export interface ElementScrollerProps {
+  scrollKey: string;
+}
+
+export class ElementScroller extends React.Component<ElementScrollerProps> { }
